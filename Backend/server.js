88108d3f30fdddf8eac0a24dd70215642ee2b2b2
Jeyaram A <ramram13052001@gmail.com>
2026-08-13 13:06:49 +0530

    payu

--- Backend/server.js
+++ Backend/server.js
@@ -7,11 +7,15 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'https://www.yencodetechnologies.com',
-      "https://yencodeweb.octosofttechnologies.in"
+  'https://yencodetechnologies.com',
+  'https://yencodeweb.octosofttechnologies.in',
+  'https://test.payu.in',
+  'https://secure.payu.in'
 ];
 
 app.use(cors({
   origin: (origin, callback) => {
+    console.log('Incoming request origin:', origin);
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
@@ -182,7 +186,7 @@ app.post('/api/payment/payu-initiate', requireAuth, (req, res) => {
     const mail = (email || '').trim();
     const ph = (phone || '9999999999').replace(/\D/g, '').slice(0, 10) || '9999999999';
 
-     const hashString = `${key}|${txnid}|${amount}|${productinfo}|${firstname}|${email}|${udf1}|${udf2}|${udf3}|${udf4}|${udf5}||||||${salt}`;
+     const hashString = `${key}|${txnid}|${formattedAmount}|${productinfo}|${fname}|${mail}||||||||||${salt}`;
     const hash = crypto.createHash('sha512').update(hashString).digest('hex');
 
     const backendUrl = (process.env.APP_BASE_URL || 'http://localhost:1524').replace(/\/$/, '');
@@ -200,7 +204,8 @@ app.post('/api/payment/payu-initiate', requireAuth, (req, res) => {
         firstname: fname,
         email: mail,
         phone: ph,
-      
+         surl: `${backendUrl}/api/payment/payu-success`,
+        furl: `${backendUrl}/api/payment/payu-failure`,
         hash,
       },
     });
