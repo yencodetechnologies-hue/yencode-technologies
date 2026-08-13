f3ee84401d2e4c748e7f40e970db813b2edc4f83
Jeyaram A <ramram13052001@gmail.com>
2026-08-13 09:58:36 +0530

    payu

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("MongoDB Connected Successfully");
 
